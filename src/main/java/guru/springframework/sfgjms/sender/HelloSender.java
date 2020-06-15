8d0872080ac1c4bc97dcd17237f24df9df5a4641
Mičulka Patrik <patrik.miculka@sykora.cz>
2020-06-15 21:09:23 +0200

    Send and receive a message

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,23 +1,29 @@
 package guru.springframework.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HelloSender {
 
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper objectMapper;
 
 	@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
-		System.out.println("I'm sending a message");
 		HelloWorldMessage message = HelloWorldMessage
 				.builder()
 				.id(UUID.randomUUID())
@@ -26,6 +32,28 @@ public class HelloSender {
 
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-		System.out.println("Message sent");
+	}
+
+	@Scheduled(fixedRate = 2000)
+	public void sendAndReceiveMessage() throws JMSException {
+		HelloWorldMessage message = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("Hello")
+				.build();
+
+		Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_REC_QUEUE, session -> {
+			Message helloMessage;
+			try {
+				helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+				helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+				log.info("Sending hello");
+				return helloMessage;
+			} catch (JsonProcessingException e) {
+				log.error(e.getMessage(), e);
+				throw new JMSException("boom");
+			}
+		});
+		log.info(receivedMessage.getBody(String.class));
 	}
 }
